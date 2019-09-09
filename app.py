f92ceca11f5a7ab85e46f9345ebf919632b8b717
Mai <mai.th.hoang@gmail.com>
2019-09-09 16:15:16 +0700

    updatedversion

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_login import UserMixin, LoginManager, login_required, current_user,login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
 
 
 
@@ -13,7 +14,7 @@ loginmanager = LoginManager(app)
 FLASK_DEBUG=1
 POSTGRES = {
       'user': 'Mai',
-      'pw': '',
+      'pw': 'Codeordie2019',
       'db': 'mai',
       'host': 'localhost',
       'port': 5432,
@@ -23,7 +24,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:\
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY']='Zingo'
 db = SQLAlchemy(app)
-db.create_all()
+migrate = Migrate(app,db)
+
 
 
 class Post(db.Model):
@@ -33,12 +35,9 @@ class Post(db.Model):
   author_name=db.Column(db.String(50),default='anonymous',nullable=False)
   created_on = db.Column(db.DateTime, default=db.func.now())
   updated_on = db.Column(db.DateTime, default=db.func.now())
+  view_count = db.Column(db.Integer, default=0)
+  flags = db.relationship('Flags', backref="post", lazy="dynamic")
 
-class PostForm(FlaskForm):
-  title=StringField('Title', validators=[DataRequired()])
-  body=StringField('Text', validators=[DataRequired()])
-  author_name=StringField('Name', validators=[DataRequired()])
-  submit=SubmitField('Create Post')
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -46,14 +45,29 @@ class User(UserMixin, db.Model):
   last_name = db.Column(db.String(120))
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(120), nullable=False)
+  flags = db.relationship('Flags', backref="user", lazy="dynamic")
   def set_password(self, password):
     self.password_hash = generate_password_hash(password)
 
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
 
+class Flags(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  post_id =  db.Column(db.Integer, db.ForeignKey('post.id'))
+
 
 db.create_all()
+
+
+class NewForm(FlaskForm):
+  title=StringField('Title', validators=[DataRequired()])
+  body=StringField('Text', validators=[DataRequired()])
+  author_name=StringField('Name', validators=[DataRequired()])
+  submit=SubmitField('Create Post')
+
+
 
 
 @loginmanager.user_loader
@@ -62,15 +76,40 @@ def load_user(id):
 
 @app.route('/')
 def hello_world():
-    return 'home!'
+    
+    return redirect(url_for('profile'))
 
-@app.route('/posts')
-def posts():
-    return render_template('posts.html')
+@app.route('/posts/<int:post_id>')
+def posts(post_id):
+  data=Post.query.filter_by(id=post_id).first()
+  check=Post.query.filter_by(id=post_id).first()
+  check.view_count += 1
+  db.session.commit()
+  return render_template('posts.html', data=data)
 
-@app.route('/create')
+@app.route('/repost/<post_id>')
+def report(post_id):
+    report=Flags(user_id=current_user.id, post_id=post_id)
+    db.session.add(report)
+    db.session.commit()
+    flash('Your report has been sent', 'success')
+  
+    return redirect(url_for('profile'))
+
+@app.route('/create', methods=['POST', 'GET'])
+@login_required
 def create_post():
-  form=PostForm()
+  form = NewForm()  ###### FAIL
+  if request.method == 'POST':  
+    if form.validate_on_submit():
+      new_post = Post(title=form.title.data,
+                        body=form.body.data,
+                        author_name=form.author_name.data)
+      db.session.add(new_post)
+      db.session.commit()
+      return redirect(url_for('posts'))
+    else:
+      flash('Please, fill in the missing space')
   return render_template('create.html', form=form)
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -90,6 +129,8 @@ def login():
 def logout():
   logout_user()
   return redirect(url_for('login'))
+
+
 
 @app.route('/signup',methods=['POST', 'GET'])
 def signup():
@@ -111,7 +152,8 @@ def signup():
 @app.route('/profile')
 @login_required
 def profile():
-  return render_template('profile.html')
+  posts = Post.query.all()
+  return render_template('profile.html', posts=posts)
 
 
 if (__name__) == '__main__':
---
+++ static/app.js
@@ -0,0 +1,5 @@
+console.log('hi')
+
+function report() {
+    console.log('hi inside with bitna')
+}
